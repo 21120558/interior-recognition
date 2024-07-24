016d089916d65b1418357a13088de97bcd64cc59
hoaithi <phunghoaithi137@gmail.com>
2024-07-24 09:19:17 +0700

    refactor: batch size

--- vgg16/training.py
+++ vgg16/training.py
@@ -7,7 +7,7 @@ train_data_dir = '../data/train'
 validation_data_dir = '../data/test'
 
 img_width, img_height = 224, 224
-batch_size = 32
+batch_size = 8
 
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
@@ -15,7 +15,6 @@ train_datagen = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -23,11 +22,6 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical')
 
-validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='categorical')
 
 
 from keras.layers import Dense, GlobalAveragePooling2D
@@ -57,8 +51,6 @@ callbacks = [checkpoint, early_stopping]
 model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // batch_size,
-    validation_data=validation_generator,
-    validation_steps=validation_generator.samples // batch_size,
     epochs=50,
     callbacks=callbacks)
 
